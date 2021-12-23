882557c8881acf36857b0e426e6845ae8e2ce0cb
Oleg Batischev <admin@magedev.ru>
2021-12-23 14:24:57 +0300

    Modify install

--- install.sh
+++ install.sh
@@ -9,17 +9,15 @@ StartLimitIntervalSec=0
 [Service]
 Type=simple
 User=$USER
-ExecStart=/usr/bin/snarkos
+ExecStart=/usr/bin/aleo_exporter
 Restart=always
 RestartSec=3
 LimitNOFILE=10000
 [Install]
 WantedBy=multi-user.target
 EnvironmentFile=/etc/sysconfig/aleo_exporter
-ExecStart=/usr/bin/aleo_exporter
-
 " > /etc/systemd/system/aleo-prometheus-exporter.service
-
+sudo cp aleo_exporter /usr/bin/aleo_exporter
 sudo systemctl daemon-reload
 sudo systemctl enable aleo-prometheus-exporter
 sudo systemctl start aleo-prometheus-exporter
